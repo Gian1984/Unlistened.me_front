1032deba6e6f655608e5f1f75d4477d30769d074
Gianluca Tiengo <gl.tiengo@gmail.com>
2024-05-14 23:25:04 +0200

    adding some logo

--- src/views/NavigationView.vue
+++ src/views/NavigationView.vue
@@ -61,8 +61,8 @@ const sidebarOpen = ref(false)
               </TransitionChild>
               <!-- Sidebar component, swap this element with another sidebar if you like -->
               <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
-                <div class="flex h-16 shrink-0 items-center">
-                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+                <div class="flex h-32 shrink-0 items-center">
+                  <img class="h-16 w-auto" src="/images/unlistened_transparen_logo_176.png" alt="My Image"/>
                 </div>
                 <nav class="flex flex-1 flex-col">
                   <ul role="list" class="flex flex-1 flex-col gap-y-7">
@@ -106,8 +106,8 @@ const sidebarOpen = ref(false)
     <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
       <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
-        <div class="flex h-16 shrink-0 items-center">
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+        <div class="flex h-24 shrink-0 items-center">
+          <img class="h-16 w-auto" src="/images/unlistened_transparen_logo_176.png" alt="Your Company" />
         </div>
         <nav class="flex flex-1 flex-col">
           <ul role="list" class="flex flex-1 flex-col gap-y-7">
--- src/views/ResetPasswordView.vue
+++ src/views/ResetPasswordView.vue
@@ -5,7 +5,7 @@ import { XCircleIcon } from '@heroicons/vue/20/solid'
 <template>
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+      <img class="mx-auto h-32 w-auto" src="/images/unlistened_transparen_logo_176.png" alt="Your Company" />
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Sign in to your account</h2>
     </div>
 
