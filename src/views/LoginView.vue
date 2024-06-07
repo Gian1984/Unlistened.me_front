131f71708881c0cc200321b87f4a4707f4c83d52
Gianluca Tiengo <gl.tiengo@gmail.com>
2024-06-07 11:55:05 +0200

    avoiding autocomplete and using form to login and register user

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -12,51 +12,53 @@ import { useMessageStore } from '@/stores/messageStore';
       <p v-if="message" class="mt-5 text-center text-white font-bold">{{ message }}</p>
     </div>
 
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <div class="space-y-6">
-        <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-white">Email address</label>
-          <div class="mt-2">
-            <input v-model="email" id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
-
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-white">Password</label>
-            <div class="text-sm">
-              <router-link  to="/forgot_password" class="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</router-link>
+    <form  @submit.prevent="login">
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div class="space-y-6">
+          <div>
+            <label for="email" class="block text-sm font-medium leading-6 text-white">Email address</label>
+            <div class="mt-2">
+              <input v-model="email" id="email" name="email" type="email" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" required />
             </div>
           </div>
-          <div class="mt-2">
-            <input  v-model="password" id="password" name="password" type="password"  required="" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
 
-        <div v-if="errors" class="rounded-md bg-red-50 p-4">
-          <div class="flex">
-            <div class="flex-shrink-0">
-              <button v-on:click="closeAlert()" type="button">
-                <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
-              </button>
+          <div>
+            <div class="flex items-center justify-between">
+              <label for="password" class="block text-sm font-medium leading-6 text-white">Password</label>
+              <div class="text-sm">
+                <router-link  to="/forgot_password" class="font-semibold text-indigo-400 hover:text-indigo-300">Forgot password?</router-link>
+              </div>
             </div>
-            <div class="ml-3">
-              <h3 class="text-sm font-medium text-red-800">Error</h3>
-              <div class="mt-2 text-sm text-red-700">
-                <ul role="list" class="list-disc space-y-1 pl-5">
-                  <li>{{errors.error}}</li>
-                </ul>
+            <div class="mt-2">
+              <input  v-model="password" id="password" name="password" type="password"  class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" required/>
+            </div>
+          </div>
+
+          <div v-if="errors" class="rounded-md bg-red-50 p-4">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <button v-on:click="closeAlert()" type="button">
+                  <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
+                </button>
+              </div>
+              <div class="ml-3">
+                <h3 class="text-sm font-medium text-red-800">Error</h3>
+                <div class="mt-2 text-sm text-red-700">
+                  <ul role="list" class="list-disc space-y-1 pl-5">
+                    <li>{{errors.error}}</li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
+
+          <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Login</button>
+          </div>
         </div>
 
-        <div>
-          <button @click="login" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Login</button>
-        </div>
       </div>
-
-    </div>
+    </form>
   </div>
 </template>
 <script>
@@ -78,7 +80,8 @@ export default {
     },
   },
   methods: {
-    login() {
+    login(e) {
+      e.preventDefault()
       this.axios.defaults.withCredentials = true;
       this.axios.defaults.withXSRFToken = true;
 
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -9,83 +9,85 @@ import { XCircleIcon } from '@heroicons/vue/20/solid'
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Create your account</h2>
     </div>
 
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <div class="space-y-6">
-        <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-white">Username</label>
-          <div class="mt-2">
-            <input v-model="username" type="text" name="username" id="username" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" required/>
-          </div>
-        </div>
-        <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-white">Email address</label>
-          <div class="mt-2">
-            <input v-model="email" id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" required/>
-          </div>
-        </div>
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-white">Password</label>
-          </div>
-          <div class="mt-2">
-            <input v-model="password" id="password" name="password" type="password" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" required/>
-          </div>
-        </div>
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="terms" class="block text-sm font-medium leading-6"><router-link to="/terms" class="text-indigo-400 hover:text-indigo-300">Terms & conditions</router-link></label>
-          </div>
-          <div class="mt-2 flex items-center">
-            <input v-model="checked" name="terms" id="terms" type="checkbox" checked required />
-            <p id="candidates-description" class="text-white text-sm ml-2">
-              I understand and accept
-            </p>
-          </div>
-        </div>
-
-        <div v-if="errorsRegister" class="rounded-md bg-red-50 p-4">
-          <div class="flex">
-            <div class="flex-shrink-0">
-              <button v-on:click="closeAlert()" type="button">
-                <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
-              </button>
+    <form  @submit.prevent="signup">
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div class="space-y-6">
+          <div>
+            <label for="email" class="block text-sm font-medium leading-6 text-white">Username</label>
+            <div class="mt-2">
+              <input v-model="username" type="text" name="username" id="username" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" required/>
             </div>
-            <div class="ml-3">
-              <h3 class="text-sm font-medium text-red-800">Error</h3>
-              <div class="mt-2 text-sm text-red-700">
-                <ul role="list" class="list-disc space-y-1 pl-5">
-                  <li v-if="errorsRegister.password">{{errorsRegister.password[0]}}</li>
-                  <li v-if="errorsRegister.email">{{errorsRegister.email[0]}}</li>
-                </ul>
+          </div>
+          <div>
+            <label for="email" class="block text-sm font-medium leading-6 text-white">Email address</label>
+            <div class="mt-2">
+              <input v-model="email" id="email" name="email" type="email" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" required/>
+            </div>
+          </div>
+          <div>
+            <div class="flex items-center justify-between">
+              <label for="password" class="block text-sm font-medium leading-6 text-white">Password</label>
+            </div>
+            <div class="mt-2">
+              <input v-model="password" id="password" name="password" type="password" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6" required/>
+            </div>
+          </div>
+          <div>
+            <div class="flex items-center justify-between">
+              <label for="terms" class="block text-sm font-medium leading-6"><router-link to="/terms" class="text-indigo-400 hover:text-indigo-300">Terms & conditions</router-link></label>
+            </div>
+            <div class="mt-2 flex items-center">
+              <input v-model="checked" name="terms" id="terms" type="checkbox" checked required />
+              <p id="candidates-description" class="text-white text-sm ml-2">
+                I understand and accept
+              </p>
+            </div>
+          </div>
+
+          <div v-if="errorsRegister" class="rounded-md bg-red-50 p-4">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <button v-on:click="closeAlert()" type="button">
+                  <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
+                </button>
+              </div>
+              <div class="ml-3">
+                <h3 class="text-sm font-medium text-red-800">Error</h3>
+                <div class="mt-2 text-sm text-red-700">
+                  <ul role="list" class="list-disc space-y-1 pl-5">
+                    <li v-if="errorsRegister.password">{{errorsRegister.password[0]}}</li>
+                    <li v-if="errorsRegister.email">{{errorsRegister.email[0]}}</li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div v-if="empty" class="rounded-md bg-red-50 p-4">
-          <div class="flex">
-            <div class="flex-shrink-0">
-              <button v-on:click="closeAlertEmpty()" type="button">
-                <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
-              </button>
-            </div>
-            <div class="ml-3">
-              <h3 class="text-sm font-medium text-red-800">Error:</h3>
-              <div class="mt-2 text-sm text-red-700">
-                <ul role="list" class="list-disc space-y-1 pl-5">
-                  <li>{{empty}}</li>
-                </ul>
+          <div v-if="empty" class="rounded-md bg-red-50 p-4">
+            <div class="flex">
+              <div class="flex-shrink-0">
+                <button v-on:click="closeAlertEmpty()" type="button">
+                  <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
+                </button>
+              </div>
+              <div class="ml-3">
+                <h3 class="text-sm font-medium text-red-800">Error:</h3>
+                <div class="mt-2 text-sm text-red-700">
+                  <ul role="list" class="list-disc space-y-1 pl-5">
+                    <li>{{empty}}</li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
+
+          <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign up</button>
+          </div>
         </div>
 
-        <div>
-          <button @click="signup" class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Sign up</button>
-        </div>
       </div>
-
-    </div>
+    </form>
   </div>
 
 
@@ -111,7 +113,8 @@ export default {
 
   methods : {
 
-    signup() {
+    signup(e) {
+      e.preventDefault()
       this.axios.defaults.withCredentials = true;
       this.axios.defaults.withXSRFToken = true;
 
