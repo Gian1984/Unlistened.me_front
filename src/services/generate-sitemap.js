80a151aed5c94a66b2cc3e8f60e470031171eecf
Gianluca Tiengo <gl.tiengo@gmail.com>
2026-04-09 16:19:54 +0200

    fixing script sitemap generator

--- src/services/generate-sitemap.js
+++ src/services/generate-sitemap.js
@@ -1,5 +1,9 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const routesFilePath = path.resolve(__dirname, '../router/index.js');
 const sitemapOutputPath = path.resolve(__dirname, '../../../dist/sitemap.xml');
