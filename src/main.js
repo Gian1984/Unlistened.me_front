694a40fae9822895abca3e0ed63b1149c2045371
Gianluca Tiengo <gl.tiengo@gmail.com>
2026-04-11 19:19:09 +0200

    fix persistence

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
 import './assets/main.css'
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import draggable from 'vuedraggable'
-import { useAuthStore } from './stores/authStore'
 
-const pinia = createPinia()
+import draggable from 'vuedraggable';
+
+import { useAuthStore } from './stores/authStore';
+import { createPinia } from 'pinia'
+const pinia = createPinia();
+
 
 const app = createApp(App)
-app.use(pinia)
-app.use(router)
-app.component('draggable', draggable)
+app.component('draggable', draggable);
+app.use(pinia);
+app.use(router);
 
-app.mount('#app')
 
-const authStore = useAuthStore()
-authStore.initializeAuth()
+const authStore = useAuthStore();
+authStore.initializeAuth();
+
+app.mount('#app');
 
