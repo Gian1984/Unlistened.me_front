f8bb196d998d56770d6ab02c890faa49fafc326e
Gianluca Tiengo <gl.tiengo@gmail.com>
2026-04-15 21:48:22 +0200

    fix: revert to plain export for api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,33 +1,24 @@
 import axios from 'axios'
 import { handleUnauthorized } from '~/src/services/sessionHandler'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig()
-  
-  const api = axios.create({
-    baseURL: config.public.apiBaseUrl || 'https://www.unlistened.me',
-    withCredentials: true,
-    withXSRFToken: true,
-  })
-
-  api.interceptors.response.use(
-    response => response,
-    error => {
-      if (error.response?.status === 401 && !error.config?.skipAuthRedirect) {
-        handleUnauthorized(error)
-      }
-      return Promise.reject(error)
-    }
-  )
-
-  async function csrf() {
-    await api.get('sanctum/csrf-cookie')
-  }
-
-  return {
-    provide: {
-      api,
-      csrf,
-    }
-  }
+const api = axios.create({
+  baseURL: 'https://www.unlistened.me',
+  withCredentials: true,
+  withXSRFToken: true,
 })
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401 && !error.config?.skipAuthRedirect) {
+      handleUnauthorized(error)
+    }
+    return Promise.reject(error)
+  }
+)
+
+async function csrf() {
+  await api.get('sanctum/csrf-cookie')
+}
+
+export { api, csrf }
